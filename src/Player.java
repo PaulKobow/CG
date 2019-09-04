2823aca97025156036a4855328e5d8ad31859bc7
PaulKobow <dnemuyou@gmail.com>
2019-09-04 13:14:50 +0200

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,96 @@
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player {
+
+
+
+    public static void main(String args[]) {
+        Map<Point, FieldType> map = new HashMap<>();
+
+        ArrayList<BomberMan> players = new ArrayList<BomberMan>();
+
+        ArrayList<Bomb> bombs = new ArrayList<Bomb>();
+
+        Scanner in = new Scanner(System.in);
+        int width = in.nextInt();
+        int height = in.nextInt();
+        int myId = in.nextInt();
+
+        // game loop
+        while (true) {
+            for (int i = 0; i < height; i++) {
+                String row = in.next();
+                for(int j = 0; j < row.length(); j++){
+                    map.put(new Point(j, i), row.charAt(j) == '.' ? FieldType.Emtpy : FieldType.Box);
+                }
+            }
+            int entities = in.nextInt();
+            players.clear();
+            bombs.clear();
+            for (int i = 0; i < entities; i++) {
+                int entityType = in.nextInt();
+                int owner = in.nextInt();
+                int x = in.nextInt();
+                int y = in.nextInt();
+                int param1 = in.nextInt();
+                int param2 = in.nextInt();
+
+                if(entityType == 0){
+                    //Player
+                    players.add(new BomberMan(x,y,owner, param1));
+                }
+                else{
+                    //Bomb
+                    bombs.add(new Bomb(x,y,param1,owner, param2));
+                }
+            }
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            System.out.println("BOMB 6 5");
+        }
+    }
+}
+
+class BomberMan{
+    int x;
+    int y;
+    int id;
+    int bombsInInventory;
+
+    public BomberMan(int x, int y, int id, int bombsInInventory) {
+        this.x = x;
+        this.y = y;
+        this.id = id;
+        this.bombsInInventory = bombsInInventory;
+    }
+}
+
+class Bomb {
+    private int x;
+    private int y;
+    private int timer;
+    private int owner;
+    private int explosionrange;
+
+    public Bomb(int x, int y, int timer, int owner, int explosionRange) {
+        this.x = x;
+        this.y = y;
+        this.timer = timer;
+        this.owner = owner;
+        this.explosionrange = explosionRange;
+    }
+}
+
+enum FieldType {
+    Emtpy,
+    Box
+}
